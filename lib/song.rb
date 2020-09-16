c5c71797a9244082d5bee2d39d5a3d7f2ce577af
Eli Huebner <elimhuebner@gmail.com>
2020-09-16 17:45:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,11 @@ class Song
     self.all.find{|song| song.name == song_name}
   end
 
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name)
+      self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
+  end
+
 end
